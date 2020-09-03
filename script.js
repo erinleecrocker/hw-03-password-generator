a8e7eb855b8c8c680d242c4e15ac2bad6ac12d0f
Erin Crocker <erindowns1@me.com>
2020-09-02 23:15:35 -0400

    added the password lenth prompt, used an if else statement to make sure the password fell into the correct range of characters, then added confirm statments for Uppercase, Lowercase, Numbers, and special characters

--- script.js
+++ script.js
@@ -10,5 +10,41 @@ function writePassword() {
 
 }
 
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); {
+}
+
+//password criteria 
+
+function generatePassword() {
+
+  
+  var passwordLength = prompt("How many characters? Choose between 8 and 128");
+  console.log(passwordLength);
+  
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("You must choose between 8 and 128.")
+  }else {
+    confirmUppercase = confirm("Will this contain Uppercase letters?");
+    console.log(confirmUppercase);
+    confirmLowercase = confirm("Will this contain Lowercase letters?");
+    console.log(confirmLowercase)
+    confirmNumber = confirm("Will this contain numbers?");
+    console.log(confirmNumber);
+    confirmSpecialCharacter = confirm("Will this contain special characters?");
+    console.log(confirmSpecialCharacter);
+    
+    
+};
+  
+
+  //var passwordLowerCrit = prompt ("Include lowercase characters?");
+  //console.log(passwordLowerCrit)
+  //var passwordUpperCrit = prompt ("Include uppercase characters?");
+  //console.log(passwordUpperCrit)
+  //var passwordNumCrit = prompt ("Include numeric characters?");
+  //console.log(passwordNumCrit)
+  //var passwordSpecialCrit = prompt ("Include special characters?");
+  //console.log(passwordSpecialCrit)
+}
