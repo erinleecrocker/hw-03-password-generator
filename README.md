89ab3adfe4348ecf01e90749d3b601e9b3a4503a
Erin Crocker <erindowns1@me.com>
2020-09-03 00:45:02 -0400

    instead of if else statments changed to, while, for loop backs through the prompts and confirms, now all prompts and confirms loop back through if something is selected outside parameters or nothing is selected

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # hw-03-password-generator
+
+Purpose of this project is to create an application that generates a random password based on user-selected criteria.
+
+first my goal is to figure out how to do this! I was provided with the HTML, and CSS set up but now need to edit the index.js file to make it work!
+
+I started with generate password funciton, I needed a prompt asking for the password size (between 8 and 128) then I needed an alert if the user entered something outside of that range. 
+
+following this; as long as the user input a correct character amount I needed cofirms on the other criteria to know what should and shouldnt be included. 
+
+Although now im a bit confused and need to do a bit more googling before i understand how to get all these to do more than show up in the console.log.  Where do i store this information so that when the password is generated all these factors are taken in together? hmm. 
+
--- script.js
+++ script.js
@@ -19,28 +19,41 @@ generateBtn.addEventListener("click", writePassword); {
 
 function generatePassword() {
 
-  var UppercaseChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-  var LowercaseChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-  var NumberChar = [0,1,2,3,4,5,6,7,8,9]
-  var SpecialChar = []
+  var UppercaseChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  console.log(UppercaseChar);
+  var LowercaseChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  console.log(LowercaseChar);
+  var NumberChar = [0,1,2,3,4,5,6,7,8,9];
+  console.log(NumberChar);
+  var SpecialChar = ["!","?"];
+  console.log(SpecialChar);
   
-  var passwordLength = prompt("How many characters? Choose between 8 and 128");
+  // How many Characters in password prompt
+  var passwordLength = prompt("How many characters would you like your password to contain? Choose between 8 and 128");
+    // Loop back if outside of range
+    while (passwordLength < 9 || passwordLength > 129) {
+      alert("You must choose between 8 and 128");
+      var passwordLength = (prompt("How many characters would you like your password to contain?"));
+    }  
+  //if correct confirm ok or cancel for other password specifications
   console.log(passwordLength);
+  confirmUppercase = confirm("Will this contain Uppercase letters?");
+  console.log(confirmUppercase)
+  confirmLowercase = confirm("Will this contain Lowercase letters?");
+  console.log(confirmLowercase)
+  confirmNumber = confirm("Will this contain numbers?");
+  console.log(confirmNumber);
+  confirmSpecialCharacter = confirm("Will this contain special characters?");
+  console.log(confirmSpecialCharacter);
+  // Loop back if nothing is chosen
+    while (!confirmUppercase && !confirmLowercase && !confirmNumber && !confirmSpecialCharacter){
+      alert("You need to select at least one character type.");
+      confirmUppercase = confirm("Will this contain Uppercase letters?");
+      confirmLowercase = confirm("Will this contain Lowercase letters?");
+      confirmNumber = confirm("Will this contain numbers?");
+      confirmSpecialCharacter = confirm("Will this contain special characters?");
+    }
   
-  if (passwordLength < 8 || passwordLength > 128) {
-    alert("You must choose between 8 and 128.")
-  }else {
-    confirmUppercase = confirm("Will this contain Uppercase letters?");
-
-    //console.log(confirmUppercase);
-    confirmLowercase = confirm("Will this contain Lowercase letters?");
-    //console.log(confirmLowercase)
-    confirmNumber = confirm("Will this contain numbers?");
-    //console.log(confirmNumber);
-    confirmSpecialCharacter = confirm("Will this contain special characters?");
-    //console.log(confirmSpecialCharacter);
-    
-    
 };
   
 
@@ -52,4 +65,3 @@ function generatePassword() {
   //console.log(passwordNumCrit)
   //var passwordSpecialCrit = prompt ("Include special characters?");
   //console.log(passwordSpecialCrit)
-}
