03f71b75168fa78ee732be1327c972267c06b843
Erin Crocker <erindowns1@me.com>
2020-09-03 22:34:43 -0400

    finished the while loop to randomly generate the characters, and then used a substr to make the random characters produced in the loop match the user defined password length, then i returned that value at the end of the function

--- script.js
+++ script.js
@@ -15,53 +15,69 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword); {
 }
 
-//password criteria 
+//---------------- Generate Password : 
 
 function generatePassword() {
-
-  var UppercaseChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  console.log(UppercaseChar);
-  var LowercaseChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  console.log(LowercaseChar);
-  var NumberChar = [0,1,2,3,4,5,6,7,8,9];
-  console.log(NumberChar);
-  var SpecialChar = ["!","?"];
-  console.log(SpecialChar);
+  //--------------password variables for criteria 
+  var uppercaseChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  //console.log(uppercaseChar);
+  var lowercaseChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  //console.log(lowercaseChar);
+  var numberChar = [0,1,2,3,4,5,6,7,8,9];
+  //console.log(numberChar);
+  var specialChar = ["!", "@", "#", "$", "%", "^", "&","*"];
+  //console.log(specialChar);
+  var randomPassword = ""
   
-  // How many Characters in password prompt
+  // --------------How many Characters in password prompt
   var passwordLength = prompt("How many characters would you like your password to contain? Choose between 8 and 128");
-    // Loop back if outside of range
-    while (passwordLength < 9 || passwordLength > 129) {
+    //----------------- Loop back if outside of range
+    while (passwordLength < 7 || passwordLength > 129) {
       alert("You must choose between 8 and 128");
       var passwordLength = (prompt("How many characters would you like your password to contain?"));
-    }  
-  //if correct confirm ok or cancel for other password specifications
+    } 
+
+
   console.log(passwordLength);
-  confirmUppercase = confirm("Will this contain Uppercase letters?");
+  //------------Confirm Uppercase, Lowercase, Number, and Special Characters
+  confirmUppercase = confirm("Will this password contain uppercase characters?"); 
   console.log(confirmUppercase)
-  confirmLowercase = confirm("Will this contain Lowercase letters?");
+  confirmLowercase = confirm("Will this password contain lowercase characters?");
   console.log(confirmLowercase)
-  confirmNumber = confirm("Will this contain numbers?");
+  confirmNumber = confirm("Will this password contain numbers?");
   console.log(confirmNumber);
-  confirmSpecialCharacter = confirm("Will this contain special characters?");
+  confirmSpecialCharacter = confirm("Will this password contain special characters?");
   console.log(confirmSpecialCharacter);
-  // Loop back if nothing is chosen
+  //------------- Loop back if nothing is chosen
     while (!confirmUppercase && !confirmLowercase && !confirmNumber && !confirmSpecialCharacter){
       alert("You need to select at least one character type.");
-      confirmUppercase = confirm("Will this contain Uppercase letters?");
-      confirmLowercase = confirm("Will this contain Lowercase letters?");
-      confirmNumber = confirm("Will this contain numbers?");
-      confirmSpecialCharacter = confirm("Will this contain special characters?");
+      confirmUppercase = confirm("Will this password contain Uppercase letters?");
+      confirmLowercase = confirm("Will this password contain Lowercase letters?");
+      confirmNumber = confirm("Will this password contain numbers?");
+      confirmSpecialCharacter = confirm("Will this password contain special characters?");
     }
+      //---- idea: get a loop going runit until it reaches the password length have it assign each 
+      //that has been approved by confirms?
   
+  while (randomPassword.length < passwordLength) {
+    
+    if (confirmUppercase === true) {
+      randomPassword += uppercaseChar[Math.floor(Math.random() * uppercaseChar.length)];
+    }
+    if (confirmLowercase === true) {
+      randomPassword += lowercaseChar[Math.floor(Math.random() * lowercaseChar.length)];
+    } 
+    if (confirmNumber === true) {
+      randomPassword += numberChar[Math.floor(Math.random() * numberChar.length)];
+    }
+    if (confirmSpecialCharacter === true) {
+      randomPassword += specialChar[Math.floor(Math.random() * specialChar.length)]; 
+    }
+  }
+  randomPassword = randomPassword.substr(0,passwordLength)
+  return(randomPassword)
 };
-  
 
-  //var passwordLowerCrit = prompt ("Include lowercase characters?");
-  //console.log(passwordLowerCrit)
-  //var passwordUpperCrit = prompt ("Include uppercase characters?");
-  //console.log(passwordUpperCrit)
-  //var passwordNumCrit = prompt ("Include numeric characters?");
-  //console.log(passwordNumCrit)
-  //var passwordSpecialCrit = prompt ("Include special characters?");
-  //console.log(passwordSpecialCrit)
+
+
+
